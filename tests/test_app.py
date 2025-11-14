e4561be8c1698f343c48f45f4b82b14bb24a853b
Alejandro Fontal <alejandro.fontal.92@gmail.com>
2025-11-14 15:57:31 +0100

    test: replace Selenium visual baseline with smoke tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,39 @@
+import os
+
 import pytest
 from seleniumbase import BaseCase
 
 
-class VisualTest(BaseCase):
+BASE_URL = os.environ.get("E2E_BASE_URL", "http://localhost:8080")
+
+
+class NiceGUIVisibilityTests(BaseCase):
+    """SeleniumBase smoke tests to ensure critical UI blocks render."""
+
+    def open_dashboard(self) -> None:
+        self.open(BASE_URL)
+        self.wait_for_element("body")
+
     @pytest.mark.e2e
-    def test_app_runs(self):
-        # automated visual regression testing
-        # tests page has identical structure to baseline
-        # https://github.com/seleniumbase/SeleniumBase/tree/master/examples/visual_testing
-        self.open("http://localhost:8501")
-        self.wait_for_element("[data-testid=stAppViewContainer]")
-        self.assert_element("[data-testid=stAppViewContainer]")
-        self.check_window(name="app_runs", level=3)
+    def test_core_sections_are_visible(self) -> None:
+        self.open_dashboard()
+        selectors = [
+            ".status-card",
+            ".ns-settings-card",
+            ".pattern-card",
+            ".tir-select",
+            ".night-input",
+            ".ns-primary-button",
+        ]
+        for selector in selectors:
+            self.wait_for_element(selector)
+            self.assert_element_visible(selector)
+
+    @pytest.mark.e2e
+    def test_theme_toggle_switch_is_interactive(self) -> None:
+        self.open_dashboard()
+        toggle_selector = ".theme-toggle-simple .q-toggle__inner, .theme-toggle-simple .q-switch__inner"
+        self.wait_for_element(toggle_selector)
+        self.assert_element_visible(toggle_selector)
+        self.click(toggle_selector)
+        self.wait_for_element("body.light-theme")
